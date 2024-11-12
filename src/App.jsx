801945bde459bb76049259d84430cef3d98a09a2
MostafaTarekAioup <mostafatarekaioup@gmail.com>
2024-11-12 12:25:37 +0200

    add env file and add 2 way store sharing between 2 apps

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Hello from './Components/Hello'
+
+import { useOpenModule } from 'appHouse/appStore'
+import TempComp from 'appHouse/TempComp'
+
+function App() {
+  const [isOpen] = useOpenModule()
+  return (
+    <>
+      <h1>app 1</h1>
+      {isOpen && <Hello />}
+      <TempComp />
+    </>
+  )
+}
+
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 
+import dotenv from 'dotenv'
+dotenv.config()
+const isLocal = process.env.ENV === 'Local'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -9,8 +13,13 @@ export default defineConfig({
     federation({
       name: 'app1',
       filename: 'remoteEntry.js',
+      remotes: {
+        appHouse: isLocal
+          ? 'http://localhost:4173/assets/remoteEntry.js'
+          : 'https://mfe-app-house.vercel.app/assets/remoteEntry.js',
+      },
       exposes: {
-        './App1': './src/App.tsx',
+        './App1': './src/App.jsx',
         './Hello': './src/Components/Hello',
         './store': './src/store.ts',
       },
